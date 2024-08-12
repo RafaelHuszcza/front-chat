aad307cb8d516f3a325ffa760c4c7b0bd1df4fa4
rafaelhuszcza <rafaelhuszcza@gmail.com>
2024-08-12 01:36:21 -0300

    feat: create websocket logic and connection

--- src/app/app/rooms/_components/chat.tsx
+++ src/app/app/rooms/_components/chat.tsx
@@ -1,244 +1,155 @@
 'use client'
-import { useSession } from 'next-auth/react'
-import { useEffect, useState } from 'react'
 
-import { socket } from '@/socket'
+import { LoaderIcon } from 'lucide-react'
+import { redirect, useRouter } from 'next/navigation'
+import { Session } from 'next-auth'
+import { useEffect, useRef, useState } from 'react'
+
+import { useRoom } from '@/api-uses/rooms/use-room'
+import { Dialog, DialogContent } from '@/components/ui/dialog'
+import { useMessages } from '@/providers/message-provider'
+import { useWebSocket } from '@/providers/websocket-provider'
 
 import { Form } from './form'
 import { Header } from './header'
 import Messages from './messages'
 import OnlineMembersAside from './online-members-aside'
+
 export interface Member {
   name: string
   email: string
   id: string
 }
-export interface Message {
-  id: string
-  content: string
-  user: Member
-  createdAt: Date
-}
 export interface Room {
   id: string
   subject: string
 }
-export function Chat({ id }: { id: string }) {
-  const { data } = useSession()
-  if (!data) return null
-  // const [isConnected, setIsConnected] = useState(false)
-  // const [transport, setTransport] = useState('N/A')
 
-  // useEffect(() => {
-  //   if (socket.connected) {
-  //     onConnect()
-  //   }
-  //   function onConnect() {
-  //     setIsConnected(true)
-  //     setTransport(socket.io.engine.transport.name)
+export function Chat({
+  roomId,
+  session,
+}: {
+  roomId: string
+  session: Session
+}) {
+  const { createConnection, currentSocket, setCurrentSocket, reconnecting } =
+    useWebSocket()
+    const initialCheckDone = useRef(false);
+  const [members, setMembers] = useState<Member[]>([])
 
-  //     socket.io.engine.on('upgrade', (transport) => {
-  //       setTransport(transport.name)
-  //     })
-  //   }
+  const { messages, clearMessages, appendMessage } = useMessages()
+  const { data: room, isSuccess, isLoading, isError } = useRoom(roomId)
+  const router = useRouter()
+  useEffect(() => {
+    if (!room || !isSuccess) return
+    //TODO : Remover mock de membros e verificar conexão com websocket
+    // setMembers([
+    //   {
+    //     name: "Rafael Machado",
+    //     email : "rafaelhuszcza@gmail.com",
+    //     id: session.user.id,
+    //   },
+    //   {
+    //     name: "Luan Simões",
+    //     email : "luansds@gmail.com",
+    //     id: "aa",
+    //   },
+    //   {
+    //     name: "Leonardo Silva",
+    //     email : "leosinhosdsilva@gmail.com",
+    //     id: "bb",
+    //   },
+    // ])
+    const socket = createConnection(room.id, session.user.id)
+    if (socket) {
+      setCurrentSocket(socket)
+    }
+  }, [
+    session.user.id,
+    room,
+    createConnection,
+    isSuccess,
+    setCurrentSocket,
+    currentSocket,
+  ])
+  
 
-  //   function onDisconnect() {
-  //     setIsConnected(false)
-  //     setTransport('N/A')
-  //   }
+  useEffect(() => {
+    if (!initialCheckDone.current) {
+      for (let i = 0; i < messages.length; i++) {
+        if (messages[i].roomId !== roomId) {
+          clearMessages();
+          break;
+        }
+      }
+      initialCheckDone.current = true;
+    }
+  }, [messages, roomId, clearMessages]);
+  if(isError) router.push('/app/rooms/not-found')
+  if (!currentSocket) return 'Carregando...'
 
-  //   socket.on('connect', onConnect)
-  //   socket.on('disconnect', onDisconnect)
-
-  //   return () => {
-  //     socket.off('connect', onConnect)
-  //     socket.off('disconnect', onDisconnect)
-  //   }
-  // }, [])
-
-  const members: Member[] = [
-    {
-      name: 'John Doe',
-      email: 'rafael@gmail.com',
-      id: 'dasdsada1',
-    },
-    {
-      name: 'Alice Smith',
-      email: 'asdsad@gmail.com',
-      id: 'dasdsada2',
-    },
-    {
-      name: 'Rafael Machado',
-      email: 'asdsad@gmail.com',
-      id: 'clzg9fz7v0000zkofk184xsiv',
-    },
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-    {
-      name: 'teeste',
-      email: 'asdsad@gmail.com',
-      id: '21321',
-    },
-  ]
-  const messages: Message[] = [
-    {
-      id: '1',
-      content: 'Hey everyone! How is it going?',
-      user: members[0],
-      createdAt: new Date('2021-09-01T14:41:00'),
-    },
-    {
-      id: '2',
-      content: 'Hi John, doing great! How about you?',
-      user: members[2],
-      createdAt: new Date('2021-09-01T14:42:00'),
-    },
-    {
-      id: '3',
-      content: 'Doing well, thanks for asking!',
-      user: members[0],
-      createdAt: new Date('2021-09-01T14:43:00'),
-    },
-    {
-      id: '4',
-      content: 'Has anyone seen the latest episode of the show?',
-      user: members[1],
-      createdAt: new Date('2021-09-01T14:44:00'),
-    },
-    {
-      id: '5',
-      content: 'Not yet, planning to watch it tonight.',
-      user: members[3],
-      createdAt: new Date('2021-09-01T14:45:00'),
-    },
-    {
-      id: '6',
-      content: 'It was amazing! So many twists and turns.',
-      user: members[2],
-      createdAt: new Date('2021-09-01T14:46:00'),
-    },
-    {
-      id: '7',
-      content: 'Can’t wait to see it!',
-      user: members[0],
-      createdAt: new Date('2021-09-01T14:47:00'),
-    },
-    {
-      id: '8',
-      content: 'What are your weekend plans?',
-      user: members[0],
-      createdAt: new Date('2021-09-01T14:48:00'),
-    },
-    {
-      id: '9',
-      content: 'Thinking about going hiking if the weather is nice.',
-      user: members[3],
-      createdAt: new Date('2021-09-01T14:49:00'),
-    },
-    {
-      id: '10',
-      content: 'That sounds fun! I might join you.',
-      user: members[2],
-      createdAt: new Date('2021-09-01T14:50:00'),
-    },
-  ]
-  const room: Room = {
-    id: 'clzgb76zi0001uh26a04cp35o',
-    subject: 'General',
+  currentSocket.onmessage = (event) => {
+    const message = JSON.parse(event.data)
+    console.log('Received message:', message)
+    if (message.type === 'members') {
+      setMembers(message.members)
+    } else if (
+      message.type === 'message' ||
+      message.type === 'join' ||
+      message.type === 'leave'
+    ) {
+      appendMessage(message)
+    }
   }
-
+  const leaveRoom = () => {
+    clearMessages()
+    router.push('/app/rooms')
+  }
+ 
   return (
-    <div className="flex h-full w-full flex-1">
-      <OnlineMembersAside members={members} />
-      <div className="flex w-full flex-col">
-        <Header room={room} members={members} />
-        <Messages messages={messages} userId={data.user.id} />
-        <div className="pb-4 pl-4 pr-4">
-          <Form />
+    <>
+      {/* {reconnecting && (
+        <Dialog
+          open={reconnecting}
+          onOpenChange={() => router.push('/app/rooms')}
+        >
+          <DialogContent className="p-6 sm:max-w-sm">
+            <div className="flex flex-col items-center justify-center gap-4">
+              <LoaderIcon className="h-8 w-8 animate-spin text-primary" />
+              <p className="text-center text-lg font-medium">
+                Reconectando ao servidor
+              </p>
+              <p className="text-center text-sm text-muted-foreground">
+                Por favor aguarde enquanto restabelecemos a conexão
+              </p>
+            </div>
+          </DialogContent>
+        </Dialog>
+      )} */}
+      {isLoading && !reconnecting && (
+        <Dialog defaultOpen>
+          <DialogContent className="p-6 sm:max-w-sm">
+            <div className="flex flex-col items-center justify-center gap-4">
+              <LoaderIcon className="h-8 w-8 animate-spin text-primary" />
+              <p className="text-center text-lg font-medium">
+                Carregando informações
+              </p>
+            </div>
+          </DialogContent>
+        </Dialog>
+      )}
+      {isSuccess && (
+        <div className="flex h-full w-full flex-1">
+          <OnlineMembersAside members={members} />
+          <div className="flex w-full flex-col">
+            <Header room={room} members={members} leaveRoom={leaveRoom} />
+            <Messages messages={messages} userId={session.user.id} />
+            <div className="pb-4 pl-4 pr-4">
+              <Form roomId={room.id} userId={session.user.id}/>
+            </div>
+          </div>
         </div>
-      </div>
-      {/* <p>{id}</p>
-      <p>Status: {isConnected ? 'connected' : 'disconnected'}</p>
-      <p>Transport: {transport}</p> */}
-    </div>
+      )}
+    </>
   )
 }
