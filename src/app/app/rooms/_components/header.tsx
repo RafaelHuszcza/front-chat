3a8f88bf5f7b11551a75ca26b9c685c2c41f51e9
rafaelhuszcza <rafaelhuszcza@gmail.com>
2024-08-12 01:29:32 -0300

    chore: create a function to truncate text

--- src/app/app/rooms/_components/header.tsx
+++ src/app/app/rooms/_components/header.tsx
@@ -1,51 +1,86 @@
-import { CopyButton } from '@/components/copy-button'
-import { Member, Room } from './chat'
 import { Users } from 'lucide-react'
-import { Button } from '@/components/ui/button'
 import { useState } from 'react'
+
+import { CopyButton } from '@/components/copy-button'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
-import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { Member, Room } from './chat'
+import { truncateText } from '@/helpers/truncat-text'
 
-export function Header({ room, members }: { room: Room, members: Member[] }) {
+export function Header({
+  room,
+  members,
+  leaveRoom,
+}: {
+  room: Room
+  members: Member[]
+  leaveRoom: () => void
+}) {
   const [showUsers, setShowUsers] = useState(false)
   const toggleShowUsers = () => {
-    setShowUsers((prev) => !prev)}
+    setShowUsers((prev) => !prev)
+  }
+
+  
 
   return (
-    <header className="flex h-12 items-center justify-between   px-4 border-b">
-      <Button variant="ghost" className="block sm:hidden" onClick={toggleShowUsers}>
-      <Users />
+    <header className="flex h-12 items-center justify-between   border-b px-4">
+      <Button
+        variant="ghost"
+        className="block sm:hidden"
+        onClick={toggleShowUsers}
+      >
+        <Users />
       </Button>
       <Dialog open={showUsers} onOpenChange={toggleShowUsers}>
-      <DialogContent className="flex w-auto flex-col rounded-sm overflow-auto max-h-[70%]">
-        <DialogHeader>
-          <DialogTitle className="text-center">Lista de membros</DialogTitle>
-        </DialogHeader>
-        <main className="flex flex-col gap-1 overflow-y-auto overflow-x-hidden p-4">
-        {members.map((member) => (
-          <div key={member.id} className="flex h-12 items-center gap-2">
-            <Avatar className="h-8 w-8">
-              <AvatarFallback>{member.name[0]}</AvatarFallback>
-            </Avatar>
-            <div >
-              <div className="font-medium">{member.name}</div>
-              <div className="text-sm text-muted-foreground">
-                {member.email}
+        <DialogContent className="flex max-h-[70%] w-auto flex-col overflow-auto rounded-sm p-8">
+          <DialogHeader>
+            <DialogTitle className="text-center">Lista de membros - {members.length}</DialogTitle>
+          </DialogHeader>
+          <main className="flex flex-col gap-1 overflow-y-auto overflow-x-hidden p-4">
+            {members.map((member) => (
+              <div key={member.id} className="flex h-12 items-center gap-2">
+                <Avatar className="h-8 w-8">
+                  <AvatarFallback>{member.name[0]}</AvatarFallback>
+                </Avatar>
+                <div>
+                  <div className="font-medium">{member.name}</div>
+                  <div className="text-sm text-muted-foreground">
+                    {member.email}
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
-      </main>
-      </DialogContent>
-    </Dialog>
+            ))}
+          </main>
+        </DialogContent>
+      </Dialog>
+      <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+        <h3 className="w-full">{truncateText(room.subject, 10)}</h3>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{room.subject}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
       
-      <h3>Assunto: {room.subject}</h3>
-      <CopyButton  variant="ghost" text="Copiar id" value={room.id} />
+      <CopyButton variant="ghost" text="Copiar id" value={room.id} />
+      <Button variant="destructive" onClick={leaveRoom}>
+        Sair
+      </Button>
     </header>
   )
 }
--- src/app/app/rooms/_components/rooms-card.tsx
+++ src/app/app/rooms/_components/rooms-card.tsx
@@ -6,8 +6,14 @@ import { useDeleteRoom } from '@/api-uses/rooms'
 import { CopyButton } from '@/components/copy-button'
 import { DeleteModal } from '@/components/delete-modal'
 import { Button } from '@/components/ui/button'
-
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 import { Room } from '../_data/schema'
+import { truncateText } from '@/helpers/truncat-text'
 
 interface RoomsCardProps {
   room: Room
@@ -18,10 +24,22 @@ export function RoomsCard({ room }: RoomsCardProps) {
 
   return (
     <div className="flex h-60  w-52   flex-col gap-2 rounded-md border p-4 shadow-md">
-      <h3 className="font-bol text-center text-lg">{room.subject}</h3>
+     
+      <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+        <h3 className="font-bol text-center text-lg">{truncateText(room.subject, 15)}</h3>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{room.subject}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
 
       <Button asChild>
-        <Link href={`/app/rooms/${room.id}`}>Entrar na Sala</Link>
+        <Link href={`/app/rooms/${room.id}`} scroll={false}>
+          Entrar na Sala
+        </Link>
       </Button>
 
       <CopyButton variant="outline" text="Copiar id da sala" value={room.id} />
